4d3ac1c5bda316d95b54a4237947d93ddab97465
Svetlana_Shcherbakova <shchalal@mail.ru>
2025-08-16 16:45:39 +0300

    Перенесли файл health в другую папку

--- src/__tests__/test.health.js
+++ src/__tests__/test.health.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import getHealthStatus from "../health.js";
+import getHealthStatus from "../js/health.js";
 
 describe("getHealthStatus", () => {
   test("возвращает healthy при health > 50 (например, 90)", () => {
